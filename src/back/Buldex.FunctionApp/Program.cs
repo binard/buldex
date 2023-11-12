711031d7bcc3c5d86771915160e0bb1080791829
Jean-Baptiste BINARD <jean-baptiste.binard-ext@syfadis.fr>
2023-11-12 18:46:25 +0100

    back(apiresult): set api result in camel case

--- src/back/Buldex.FunctionApp/Program.cs
+++ src/back/Buldex.FunctionApp/Program.cs
@@ -3,6 +3,8 @@ using Buldex.Domain.Repositories;
 using Buldex.Infrastructure.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Text.Json.Serialization;
+using System.Text.Json;
 
 var builder = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults();
@@ -11,6 +13,11 @@ builder.ConfigureServices(services =>
 {
     services.AddTransient<ITripRepository, TripRepository>();
     services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetAllTripsQuery).Assembly));
+    services.Configure<JsonSerializerOptions>(options =>
+    {
+        options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        options.Converters.Add(new JsonStringEnumConverter());
+    });
 });
 
 var host = builder.Build();
